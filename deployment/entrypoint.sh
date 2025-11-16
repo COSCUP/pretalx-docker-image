7e4670e6aabcde169b0b607b6a85b6e5a641c69e
yoyo930021 <yoyo930021@gmail.com>
2025-11-16 15:19:06 +0800

    feat: add rebuild command with npm install during upgrade

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -70,6 +70,7 @@ case "$1" in
         wait_for_db
         python -m pretalx check --deploy
         python -m pretalx migrate --noinput
+        python -m pretalx rebuild --npm-install
         ;; 
     "manage")
         wait_for_db
